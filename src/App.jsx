71589fe5d025c9921b572316f5efead8695be32a
Aryan Singh <aryn.snh@gmail.com>
2025-08-21 03:59:48 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{maxWidth: 480, margin: '0 auto', padding: 16}}>
+      <h1 style={{fontSize: 24, fontWeight: 700}}>MBA HRM BATCH OF 25-27</h1>
+      <p>Welcome. Use /admin to manage tasks, /survey/:taskId to collect responses.</p>
+    </main>
+  )
+}
